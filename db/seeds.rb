0a406620d58542fd2b7a9c470fe163001880c945
abhideveloper28 <abhi.developer28@gmail.com>
2019-04-28 18:33:26 +0530

    Faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create 10 User
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+# Create 5 Groups
+5.times do
+  Group.create!(
+    name: Faker::Educator.subject + ' Group'
+  )
+end
+
+user_ids = User.ids # collect all user's id
+group_ids = Group.ids # collect all group's id
+
+5.times do |n|
+  group_id = group_ids[n] # get a group id
+
+  # Create a event of this group there user role is organizer
+  Event.create!(
+    user_id: user_ids.sample, group_id: group_id, role: 0
+  )
+  # Create other events of this group there user role is presenter or participant
+  (1..8).to_a.sample.times do
+    event = Event.new(
+      user_id: user_ids.sample, group_id: group_id, role: (1..2).to_a.sample
+    )
+    event.save if event.valid?
+  end
+end
