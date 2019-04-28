2c33f08f6ef4491e7ea93a02bd0322a43542102d
abhideveloper28 <abhi.developer28@gmail.com>
2019-04-28 15:43:27 +0530

    Add validation message on event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :group
   # validation
-  validates_uniqueness_of :role, scope: %i[user_id group_id]
+  validates_uniqueness_of :role, scope: %i[user_id group_id], message: "User already exist in this event."
 
   enum role: [:organizer, :presenter, :participant]
 end
