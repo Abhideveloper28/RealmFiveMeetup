624536007c8031e36291d798a1b9489aca9d5216
abhideveloper28 <abhi.developer28@gmail.com>
2019-04-29 21:47:38 +0530

    Group controller test case

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
+  describe "GET index" do
+    it "assigns @groups" do
+      group = FactoryBot.create(:group)
+      get :index
+      expect(assigns(:groups)).to eq([group])
+    end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 end
